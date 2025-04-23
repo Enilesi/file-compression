ab8d55e014d13cc226d5264d5dbd5fec406d9208
Enilesi <miriam.iseline@gmail.com>
2025-04-24 01:43:08 +0300

    deleted compression results, added printEncoded

--- proj.java
+++ proj.java
@@ -1,7 +1,10 @@
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -34,22 +37,13 @@ class FileCompresser{
         }
     }
 
-    private static class CompressionResult {
-        HufmanNode huffmanCodes;
-        String Bits;
-
-        public CompressionResult(String bits, HufmanNode tree) {
-            this.huffmanCodes = tree;
-            this.Bits = bits;
-        }
-    }
-
-    public static void encode(String inputFile) {
+    public static void encode(String inputFile, String outputFile) {
         HashMap<Byte, Integer> freq = new HashMap<>();
     
-        try (FileInputStream fis = new FileInputStream(inputFile)) {
+        try (FileInputStream fin = new FileInputStream(inputFile)) {
             int b;
-            while ((b = fis.read()) != -1) {
+            while ((b = fin.read()) != -1) {
+
                 byte byteVal = (byte) b;
                 freq.put(byteVal, freq.getOrDefault(byteVal, 0) + 1);
             }
@@ -64,7 +58,80 @@ class FileCompresser{
 
         HufmanNode root = buildHufmanTree(freq);
 
-  
+        String[] st = new String[R];
+        getHuffmanCodeTable(st, root, "");
+
+        printEncoded(inputFile,st,freq,outputFile);
+
+        
+    }
+
+
+    private static void printEncoded(String inputFile, String [] st,HashMap<Byte, Integer> freq, String outputFile){
+        try (DataOutputStream fout = new DataOutputStream(new FileOutputStream(outputFile))) {
+            int freqSize = freq.size(); 
+            fout.writeInt(freqSize); 
+            
+            for (Map.Entry<Byte, Integer> f: freq.entrySet()){
+                fout.writeChar(f.getKey());
+                fout.writeInt(f.getValue());
+                
+            }
+
+            long bitsSize = 0;
+            long bitsSizePos = fout.size(); 
+            fout.writeLong(0);
+
+            FileInputStream fin = new FileInputStream(inputFile);
+            int b;
+            int bitNr = 0;
+            byte bitBuff = 0;
+
+            while((b=fin.read())!=-1){
+                byte byteVal =(byte) b;
+
+                String huffCode = st[byteVal & 0xFF];
+                
+                for(char c: huffCode.toCharArray()){
+                    bitBuff=(byte)((bitBuff<<1) | (c - '0'));
+                    bitNr++;
+                    bitsSize++;
+
+                    if(bitNr==8){
+                        fout.writeByte(bitBuff);
+                        bitNr= 0;
+                        bitBuff = 0;
+                    }
+                }
+            }
+
+
+        if (bitNr > 0) {
+            bitBuff = (byte)(bitBuff << (8 - bitNr));
+            fout.writeByte(bitBuff);
+        }
+
+
+        RandomAccessFile raf = new RandomAccessFile(outputFile, "rw");
+        raf.seek(bitsSize);
+        raf.writeLong(bitsSizePos);
+        raf.close();
+            System.out.println("Binary data written to output");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void getHuffmanCodeTable(String[] st, HufmanNode x, String s) {
+        if (!x.isLeaf()) {
+            getHuffmanCodeTable(st, x.left, s + '0');
+            getHuffmanCodeTable(st, x.right, s + '1');
+        } else {
+            st[x.ch] = s;
+
+            System.out.println("Code of " + x.ch + " is " + s);
+        }
     }
 
     private static HufmanNode buildHufmanTree(HashMap<Byte, Integer> freq) {
@@ -83,16 +150,15 @@ class FileCompresser{
         return pq.remove();
     }
 
-    
-    
+
 
 public static void main(String[] args) {
-    if (args.length < 1) {
-        System.out.println("Usage: java FileCompresser <input-file>");
+    if (args.length < 2) {
+        System.out.println("Inncorect nr of args, expecting: <input-file> <output-file>");
         return;
     }
 
-    encode(args[0]);
+    encode(args[0],args[1]);
 }
 
 }
